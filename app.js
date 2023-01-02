57cda19a8db8935d0a12a52b1036df12870aac4a
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2023-01-02 11:37:01 +0100

    QUEST 8

--- app.js
+++ app.js
@@ -15,23 +15,27 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const usersHandlers = require("./usersHandlers");
-const { hashPassword, verifyPassword } = require("./auth");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.post("/api/users", hashPassword, usersHandlers.postUser);
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-app.put("/api/users/:id", validateUser, usersHandlers.updateUser);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-app.delete("/api/users/:id", usersHandlers.deleteUser);
+
 app.post(
   "/api/login",
   usersHandlers.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
+
+app.use(verifyToken); // following routes will need the token to be executed
+
+app.post("/api/movies", verifyToken, validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.post("/api/users", hashPassword, usersHandlers.postUser);
+app.put("/api/users/:id", validateUser, usersHandlers.updateUser);
+app.delete("/api/users/:id", usersHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
